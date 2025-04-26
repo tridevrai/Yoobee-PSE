f322893a0863dbb6eeb8e68b57ebec5fbf7b1f3a
Tridev Rai <tridev.rai@renegadeinsurance.com>
2025-04-27 09:28:15 +1200

    refactor: return 1 also for factorial of 1 and rename

--- PSE/Week_2/Activity_3_use_of_class.py
+++ PSE/Week_2/Activity_3_use_of_class.py
@@ -1,14 +1,13 @@
 #Develop a Factorial project with at least one class
 
-class Factorial:
-    number = 1
-    
+class FactorialCalculator:
+        
     def __init__(self, number):
         self.number = number
         
     def factorial_using_for_loop(self):
         print(f"inside the function with number: {self.number}")
-        if self.number == 0:
+        if self.number == 0 or self.number == 1:
             return 1
 
         factorial = 1
@@ -16,10 +15,11 @@ class Factorial:
         for i in range(1, number + 1,1):
             print(f"inside the loop with i: {i}")
             factorial = factorial * i
+            
         return factorial
 
 
 number = abs(int(input("Enter a positive integer number: ")))
-factorial = Factorial(number)
+factorial = FactorialCalculator(number)
 
 print(f"The factorial of {number} is {factorial.factorial_using_for_loop()}")
