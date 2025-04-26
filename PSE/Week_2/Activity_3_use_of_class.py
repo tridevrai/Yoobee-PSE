4a168dd1d20f346a3b291d333cc767025964ffcb
Tridev Rai <tridev.rai@renegadeinsurance.com>
2025-04-27 09:48:13 +1200

    feat: adding Prime calculator

--- PSE/Week_2/Activity_3_use_of_class.py
+++ PSE/Week_2/Activity_3_use_of_class.py
@@ -18,8 +18,34 @@ class FactorialCalculator:
             
         return factorial
 
+class PrimeNumberChecker:
+    def __init__(self,number):
+        self.number = number
+
+    def is_prime(self):
+        if self.number < 0:
+            return False
+        if self.number == 0 or self.number == 1:
+            return False
+        else:
+            factors_counter = 1
+            
+            for i in range(2, self.number+1):
+                if (self.number % i == 0):
+                    factors_counter = factors_counter + 1
+                if(factors_counter > 2):
+                    return False
+            
+            if(factors_counter == 2):
+                return True
+            else:
+                return False
+
 
 number = abs(int(input("Enter a positive integer number: ")))
+
 factorial = FactorialCalculator(number)
+prime_number = PrimeNumberChecker(number)
 
 print(f"The factorial of {number} is {factorial.factorial_using_for_loop()}")
+print(f"The number {number} is prime: {prime_number.is_prime()}")
