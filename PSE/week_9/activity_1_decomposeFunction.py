0e18b32b81137c62029735387e74780d47a568e9
Tridev Rai <tridev.rai@renegadeinsurance.com>
2025-06-14 12:39:45 +1200

    feat: add main and add comment

--- PSE/week_9/activity_1_decomposeFunction.py
+++ PSE/week_9/activity_1_decomposeFunction.py
@@ -19,8 +19,16 @@ def write_to_log_file(log_message):
     with open("rental_log.txt", "a") as log_file:
         log_file.write(log_message)
 
+    """
+    Decomposed the function into two smaller sub functions.
+    1st to build the log message
+    2nd to write the log message to the file
+    This helps to make the code more readable and easier to understand due to the separation of concerns.
+    """
 def log_event(event_type, user_id, message):
     log_message = build_log_message(event_type, user_id, message)
     write_to_log_file(log_message)
 
-log_event("login", "123456", "User logged in")
+
+if __name__ == "__main__":
+    log_event("login", "123456", "User logged in")
